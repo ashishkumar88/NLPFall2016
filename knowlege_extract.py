b429510bc9033bfccc821dd4c87bbe80a310a64b
Ashish Kumar <ashishkmr@hotmail.com>
2016-11-28 23:26:40 -0700

    Update knowlege_extract.py

--- knowlege_extract.py
+++ knowlege_extract.py
@@ -69,18 +69,8 @@ def breakConn(list):
     return d
 
 
-for line in file:
-    if line == "\n":
-        #print breakConn(temp_list)
-        #print '\n\n\n'
-        parse_list.append(breakConn(temp_list))
-        temp_list = []
-    else:
-        line = line.replace("\n","")
-        temp_list.append(line)
-
-#print parse_list
-for item in parse_list:
+def createKnowledge(item):
+	print "here"
 	reg_1F = re.compile('^nsubj.*')
 	reg_2F = re.compile('^(dobj|case|iobj).*')
 	reg_3F = re.compile('^neg.*')
@@ -97,20 +87,20 @@ for item in parse_list:
     
 	if(len(res1F)==0 or len(res2F)==0 or len(res1E)==0 or (len(res2E)==0 and len(res3E)==0) ):	
 		print 'Missing dep,Knowledge not created!!'
-		continue
+		return
 	nsubj_1F_verb = res1F[0].split(',')[0].split('(')[1].split('-')[0]
 	nsubj_1F_agent = res1F[0].split(',')[1].split('-')[0]
 
 	if len(res2F) == 0:
 		print 'dobj does not exist!!'
-		continue
+		return
 	dobj_2F_tag = res2F[0].split('(');
 	if(dobj_2F_tag[0] == 'case'):
 		dobj_2F_object = res2F[0].split(',')[0].split('(')[1].split('-')[0]
 		dobj_2F_verb = res2F[0].split(',')[1].split('-')[0]
 	else:
-		dobj_2F_object = res2F[0].split(',')[0].split('(')[1].split('-')[0]
-		dobj_2F_verb = res2F[0].split(',')[1].split('-')[0]
+		dobj_2F_verb = res2F[0].split(',')[0].split('(')[1].split('-')[0]
+		dobj_2F_object = res2F[0].split(',')[1].split('-')[0]
 	
 			   
 	nsubj_1E_verb = res1E[0].split(',')[0].split('(')[1].split('-')[0]
@@ -118,6 +108,7 @@ for item in parse_list:
 	
 	advcl_2E_verb = res2E[0].split(',')[0].split('(')[1].split('-')[0]
 	advcl_2E_prop = res2E[0].split(',')[1].split('-')[0]
+	prop = "NULL"
 	if len(res3E) != 0:
 		dobj_3E_verb = res3E[0].split(',')[0].split('(')[1].split('-')[0]
 		dobj_3E_object = res3E[0].split(',')[1].split('-')[0]
@@ -125,7 +116,10 @@ for item in parse_list:
 			prop =  dobj_3E_object
 	else:
 		prop = advcl_2E_prop    
-
+	
+	if prop == "NULL":
+		print "object not found for action"
+		return
 	agent = nsubj_1F_agent
 	object = dobj_2F_object
 	action = nsubj_1F_verb
@@ -140,3 +134,18 @@ for item in parse_list:
 		fileOut.write(str+'\n')
 	else:
 	        print 'knowledge not created!!'
+
+for line in file:
+    if line == "\n":
+        #print breakConn(temp_list)
+        #print '\n\n\n'
+        parse_list.append(breakConn(temp_list))
+        temp_list = []
+    else:
+        line = line.replace("\n","")
+        temp_list.append(line)
+
+
+#print parse_list
+for item in parse_list:
+	createKnowledge(item)
